3d476a2d3f1944493da6a4a70b2681400299645e
wizardcc <1057709293@qq.com>
2017-12-06 14:24:57 +0800

    update

--- public/javascripts/common_form_test.js
+++ public/javascripts/common_form_test.js
@@ -1,6 +1,6 @@
 // 全局变量a和b，分别获取用户框和密码框的value值
-var a = document.getElementsByTagName("input")[0].value;
-var b = document.getElementsByTagName("input")[1].value;
+var a = document.getElementsByTagName("input").user.value;
+var b = document.getElementsByTagName("input").password.value;
 
 //用户框失去焦点后验证value值
 function oBlur_1() {
@@ -42,11 +42,19 @@ function oFocus_2() {
 //若输入框为空，阻止表单的提交
 function submitTest() {
     if (!a && !b) { //用户框value值和密码框value值都为空
-        document.getElementById("remind_1").innerHTML = "请输入用户名！";
+        document.getElementById("remind_1").innerHTML = "请输入工号！";
         document.getElementById("change_margin_1").style.marginBottom = 1 + "px";
-        document.getElementById("remind_2").innerHTML = "请输入密码！";
+        document.getElementById("remind_2").innerHTML = "请输入姓名！";
         document.getElementById("change_margin_2").style.marginBottom = 1 + "px";
-        document.getElementById("change_margin_3").style.marginTop = 2 + "px";
+        document.getElementById("remind_3").innerHTML = "请输入部门！";
+        document.getElementById("change_margin_3").style.marginBottom = 1 + "px";
+        document.getElementById("remind_4").innerHTML = "请输入车牌号！";
+        document.getElementById("change_margin_4").style.marginBottom = 1 + "px";
+        document.getElementById("remind_5").innerHTML = "请输入起始时间！";
+        document.getElementById("change_margin_5").style.marginBottom = 1 + "px";
+        document.getElementById("remind_6").innerHTML = "请输入结束时间！";
+        document.getElementById("change_margin_6").style.marginBottom = 1 + "px";
+        document.getElementById("change_margin_7").style.marginTop = 1 + "px";
         return false; //只有返回true表单才会提交
     } else if (!a) { //用户框value值为空
         document.getElementById("remind_1").innerHTML = "请输入用户名！";
